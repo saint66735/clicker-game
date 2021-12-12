acba7ca60fac9dd466e19bba4f52626ddf7e8cbb
Mykolas <saint667353@gmail.com>
2021-12-12 22:41:32 +0200

    working on making shit work

--- clicker/Assets/MenuManager.cs
+++ clicker/Assets/MenuManager.cs
@@ -1,20 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MenuManager : MonoBehaviour
 {
     public GameObject SettingsPanel;
     public GameObject ModsPanel;
     public GameObject StartPanel;
+    public Save save = new Save();
     bool inGame = false;
     private void Start()
     {
         DontDestroyOnLoad(this.gameObject);
-    }
-    public void OnStartGame()
-    {
-
     }
     public void OnExitGame()
     {
@@ -24,19 +22,33 @@ public class MenuManager : MonoBehaviour
     {
 
     }
-    public void OnLoadGame()
+    public void OnSettingsApply()
     {
 
     }
+    public void OnLoadGame()
+    {
+        SceneManager.LoadScene(1);
+        save.LoadFile();
+    }
     public void OnStartNewGame()
     {
-
+        SceneManager.LoadScene(1);
+        ClearSaveData();
+    }
+    public void ClearSaveData()
+    {
+        save.NewGame();
     }
     public void OnSelectModsMenu()
     {
 
     }
     public void OnSelectMod()
+    {
+
+    }
+    public void Setup()
     {
 
     }
