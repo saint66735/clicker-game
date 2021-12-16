88c798d74ef68d676937fa7e2843f05914c25037
Raviolliox <dominykass2000@gmail.com>
2021-12-16 23:46:38 +0200

    aa

--- clicker/Assets/Scripts/DisplayCoins.cs
+++ clicker/Assets/Scripts/DisplayCoins.cs
@@ -8,22 +8,30 @@ public class DisplayCoins : MonoBehaviour
 {
     public Text yenCoinsText;
     public ClickLogic A;
+
+    public passiveIncome B;
     public double roundUp;
     public double roundUp2;
+    public double roundUp3;
     public Text clickText;
+    public Text secondText;
     // Start is called before the first frame update
     void Start()
     {
         A = FindObjectOfType<ClickLogic>();
+        B = FindObjectOfType<passiveIncome>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        roundUp = System.Math.Round(GameManager2.instance.score, 1);
+        roundUp = System.Math.Round(GameManager2.instance.score, 2);
         yenCoinsText.text = roundUp.ToString();
 
-        roundUp2 = System.Math.Round(A.increase * A.Active_Kof, 1);
+        roundUp2 = System.Math.Round(A.increase * A.Active_Kof, 2);
         clickText.text = roundUp2.ToString();
+
+        roundUp3 = System.Math.Round(B.passiveIncrease * B.Passive_Kof, 2);
+        secondText.text = roundUp3.ToString();
     }
 }
